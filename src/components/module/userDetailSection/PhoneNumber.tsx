513c321832226a3fc4141efaa3dbc05a56dcce2e
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2025-02-07 17:09:04 +0330

    design phone number

--- src/components/module/userDetailSection/PhoneNumber.tsx
+++ src/components/module/userDetailSection/PhoneNumber.tsx
@@ -13,17 +13,19 @@ function Country({ setUserDetail, userPhoneNumber, nextStep }: IInpuFill) {
   const [openModal, setOpenModal] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
 
+  // وقتی مودال باز میشه، اسکرول صفحه اصلی رو غیرفعال کن
   useEffect(() => {
     if (openModal) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = "hidden"; // غیرفعال کردن اسکرول صفحه اصلی
     } else {
-      document.body.style.overflow = "";
+      document.body.style.overflow = "auto"; // فعال کردن اسکرول صفحه اصلی
     }
     return () => {
-      document.body.style.overflow = "";
+      document.body.style.overflow = "auto"; // در هنگام ترک مودال اسکرول صفحه اصلی فعال شود
     };
   }, [openModal]);
 
+  // بسته شدن مودال وقتی که خارج از آن کلیک شود
   const handleClickOutside = (event: MouseEvent) => {
     if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
       setOpenModal(false);
@@ -43,7 +45,11 @@ function Country({ setUserDetail, userPhoneNumber, nextStep }: IInpuFill) {
         شماره تماس:
       </label>
       <div
-        className={`${nextStep && userPhoneNumber === "" ? "border-red-500" : "border-border-color dark:border-main-dark"} relative flex w-full items-center rounded-lg border-[1px] border-solid border-border-color px-4 py-[17px] xl:w-[434px] xl:gap-2`}
+        className={`${
+          nextStep && userPhoneNumber === ""
+            ? "border-red-500"
+            : "border-border-color dark:border-main-dark"
+        } relative flex w-full items-center rounded-lg border-[1px] border-solid border-border-color px-4 py-[17px] xl:w-[434px] xl:gap-2`}
       >
         <input
           type="text"
@@ -71,8 +77,9 @@ function Country({ setUserDetail, userPhoneNumber, nextStep }: IInpuFill) {
         {openModal && (
           <div
             ref={modalRef}
-            className="absolute left-3 top-14 z-10 w-40 space-y-4 overflow-hidden rounded-lg bg-white p-3 shadow-lg dark:bg-darkMoodBg"
+            className="absolute left-3 top-14 z-10 w-40 rounded-lg bg-white p-3 shadow-lg dark:bg-darkMoodBg"
           >
+            {/* لیست کشورها */}
             <div
               className="scrollbar-hidden max-h-32 overflow-y-auto"
               style={{
@@ -86,7 +93,7 @@ function Country({ setUserDetail, userPhoneNumber, nextStep }: IInpuFill) {
                   key={item.code}
                   onClick={() => {
                     setCode(item.code);
-                    setCountryFlag(item.flag); // تغییر پرچم
+                    setCountryFlag(item.flag);
                     setOpenModal(false);
                   }}
                 >
@@ -99,7 +106,9 @@ function Country({ setUserDetail, userPhoneNumber, nextStep }: IInpuFill) {
         )}
       </div>
       <span
-        className={`${nextStep && userPhoneNumber === "" ? "block" : "hidden"} -mt-1 mb-2 ml-auto h-1 text-xs font-semibold text-red-500 xl:-mt-3`}
+        className={`${
+          nextStep && userPhoneNumber === "" ? "block" : "hidden"
+        } -mt-1 mb-2 ml-auto h-1 text-xs font-semibold text-red-500 xl:-mt-3`}
       >
         پر کردن این فیلد اجباری است
       </span>
